8e93a43c1afe8ba0fa62bfae1aae1f8cdebf4dc0
itoslerhealth <itoslerhealth@gmail.com>
2025-09-06 12:42:50 +0800

    Delete netlify/functions directory

--- netlify/functions/plato-draft.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event) => {
-  try{
-    const payload = JSON.parse(event.body||'{}');
-    // TODO: call Plato API to create a draft note.
-    return { statusCode: 200, body: JSON.stringify({ok:true, echo: payload}) };
-  }catch(e){
-    return { statusCode: 200, body: JSON.stringify({ok:false, error: String(e)}) };
-  }
-};
